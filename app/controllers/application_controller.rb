b8c664f9b108bbe9fa60e6d678e23bd6e9d66834
binilsen <binil.senb@techversantinfo.com>
2022-09-22 10:52:40 +0530

    feat migrated to mongodb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  
   include ActionController::MimeResponds
 
   respond_to :json
 
-  protected
+  
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
-  end
+ 
 end
